c2f16c81f665b215932bea88986b06e8aebf3f16
nguyenlenam-18112000 <61400842+nguyenlenam-18112000@users.noreply.github.com>
2020-10-05 11:04:24 +0700

    add folder

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const morgan = require("morgan")
+const handlebars = require("express-handlebars")
+const path = require("path")
+
+var app = express();
+app.use(morgan("combined"))
+
+const port = 4000;
+
+app.engine("handlebars", handlebars())
+app.set("view engine", "handlebars")
+app.set("views", path.join(__dirname, "resource/views"))
+app.get('/', (req, res) => {
+    res.render("home")
+})
+console.log(__dirname)
+app.listen(port, () => {
+    console.log(`App is listening at http://localhost:${port}`)
+})
